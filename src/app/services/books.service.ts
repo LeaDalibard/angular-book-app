f2fc874d7ee761b82487bdda07c7c8f18041f51a
Lea Dalibard <leadalibard@gmail.com>
2021-01-12 16:04:01 +0100

    Added a method getBooks from database using firebase.database.ref + DataSnapshot

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Book} from '../models/book.model';
 import {Subject} from 'rxjs';
+import firebase from 'firebase';
+import DataSnapshot = firebase.database.DataSnapshot;
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +17,17 @@ export class BooksService {
 
   emitBooks() {
     this.booksSubject.next(this.books);
+  }
+
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  getBooks() {
+    firebase.database().ref('/books')
+      .on('value', (data: DataSnapshot) => {
+        this.books = data.val() ? data.val() : [];
+        this.emitBooks();
+      });
   }
 }
